a684cefa543ee37b1ffff34e77121cc9fc560226
WingsRZyd <91160871+WingsRZyd@users.noreply.github.com>
2022-12-11 12:48:59 +0300

    Add .host 0.0.0.0

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def hello_world():
     return "<h1>Simple Flask Application!</h1>"
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
